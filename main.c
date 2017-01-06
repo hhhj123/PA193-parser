627fd15348f65f9036acf498dac603a17a599c8e
marus-h <hatalova.maria@gmail.com>
2017-01-06 08:49:43 +0100

    added {} to one command if

--- main.c
+++ main.c
@@ -2,18 +2,23 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
-	{
-		return 1;
-	}
+    if (argc < 2)
+    {
+	return 1;
+    }
 
     int ret = json_parse(argv[1]);
 
     if(!ret)
+    {
         printf("PARSING SUCCESSFUL !\n");
+    }
     else
+    {
         printf("Problem occured!\n");
-	getchar();
+    }
+	
+    getchar();
 
-	return ret;
+    return ret;
 }
