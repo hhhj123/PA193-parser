5cd68cab55bf5c7433b17ffaac5cfc207e1703ea
marus-h <hatalova.maria@gmail.com>
2017-01-06 08:45:24 +0100

    added {} to one command if

--- json_string.c
+++ json_string.c
@@ -5,7 +5,9 @@
 int validate_string(char* in, char* out, int length)
 {
     if (strlen(in) != length)
+    {
         return INVALID_INPUT;
+    }
 
     char esc = 0;    //Boolean flag for escaping character
     char c;          //input buffer character
@@ -25,37 +27,44 @@ int validate_string(char* in, char* out, int length)
         else
         {
             if (esc)
-            switch(c)
-            {   // ALl cases of special symbols in strings
-                case 'b' :  out[i-offset] = '\b'; esc = 0; break;
-                case 'f' :  out[i-offset] = '\f'; esc = 0; break;
-                case 'n' :  out[i-offset] = '\n'; esc = 0; break;
-                case 'r' :  out[i-offset] = '\r'; esc = 0; break;
-                case 't' :  out[i-offset] = '\t'; esc = 0; break;
-                case '\\' : out[i-offset] = '\\'; esc = 0; break;
-                case '\"' : out[i-offset] = '\"'; esc = 0; break;
-                case '/' :  out[i-offset] = '/';  esc = 0; break;
-                case 'u' :
+            {
+                switch(c)
+                {   // ALl cases of special symbols in strings
+                    case 'b' :  out[i-offset] = '\b'; esc = 0; break;
+                    case 'f' :  out[i-offset] = '\f'; esc = 0; break;
+                    case 'n' :  out[i-offset] = '\n'; esc = 0; break;
+                    case 'r' :  out[i-offset] = '\r'; esc = 0; break;
+                    case 't' :  out[i-offset] = '\t'; esc = 0; break;
+                    case '\\' : out[i-offset] = '\\'; esc = 0; break;
+                    case '\"' : out[i-offset] = '\"'; esc = 0; break;
+                    case '/' :  out[i-offset] = '/';  esc = 0; break;
+                    case 'u' :
 
-                    offset--; //We are writing the same number of symbols as we read
-                    char hexChar;
-                    for (int j = 0; j < HEXA_DIGITS; j++) //checks validity of hex symbols (0-9, a-f, A-F)
-                    {
-                        hexChar = in[i+j+1];
-                        if ( hexChar < '0' || hexChar > 'f'  ||
-                            (hexChar > '9' && hexChar < 'A') ||
-                            (hexChar > 'F' && hexChar < 'a'))
-                                return INVALID_STRING;
-                    }
-                    out[i-offset-1] = '\\';
-                    out[i-offset] = 'u';
-                    esc = 0;
-                    break;
+                        offset--; //We are writing the same number of symbols as we read
+                        char hexChar;
+                        //check validity of hex symbols (0-9, a-f, A-F)
+                        for (int j = 0; j < HEXA_DIGITS; j++)
+                        {
+                            hexChar = in[i+j+1];
+                            if ( hexChar < '0' || hexChar > 'f'  ||
+                                (hexChar > '9' && hexChar < 'A') ||
+                                (hexChar > 'F' && hexChar < 'a'))
+                            {
+                                    return INVALID_STRING;
+                            }
+                        }
+                        out[i-offset-1] = '\\';
+                        out[i-offset] = 'u';
+                        esc = 0;
+                        break;
 
-                default : return INVALID_STRING;
+                    default : return INVALID_STRING;
+                }
             }
             else
+            {
                 out[i-offset] = c;
+            }
         }
     }
     return RETURN_OK;
